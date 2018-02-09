7afceedf33cbf5f88406d47f4e5c7e89bb310ff4
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2018-02-09 07:07:40 +0000

    Added membership library

---
+++ Umbraco.Membership.Library/PublishedContentExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Umbraco.Core.Models;
+
+namespace Umbraco.Membership
+{
+    public static class PublishedContentExtensions
+    {
+        public static bool IsRestricted(this IPublishedContent content, IPublishedContent member)
+        {
+            string value = content.GetProperty("restricted")?.Value?.ToString();
+            if (!bool.TryParse(value, out bool restricted) || !restricted)
+            {
+                return false;
+            }
+
+            return member == null;
+        }
+    }
+}
---
+++ Umbraco.Membership.Tests/PublishedContentExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using Moq;
+using NUnit.Framework;
+using Umbraco.Core.Models;
+
+namespace Umbraco.Membership.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public static class PublishedContentExtensionsTests
+    {
+        [TestCase(true)]
+        [TestCase(false)]
+        public static void IsRestricted_ContentDoesNotHaveProperty_AlwaysReturnsFalse(bool authorised)
+        {
+            IPublishedContent member = CreateMockMember(authorised);
+            IPublishedContent content = CreateMockContent();
+
+            bool result = content.IsRestricted(member);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public static void IsRestricted_ContentIsNotRestricted_AlwaysReturnsFalse(bool authorised)
+        {
+            IPublishedContent member = CreateMockMember(authorised);
+            IPublishedContent content = CreateMockContent(false);
+
+            bool result = content.IsRestricted(member);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestCase(true, ExpectedResult = false)]
+        [TestCase(false, ExpectedResult = true)]
+        public static bool IsRestricted_ContentIsRestricted_ReturnsInverseOfAuthorised(bool authorised)
+        {
+            IPublishedContent member = CreateMockMember(authorised);
+            IPublishedContent content = CreateMockContent(true);
+
+            bool result = content.IsRestricted(member);
+
+            return result;
+        }
+
+        private static IPublishedContent CreateMockContent(bool? restricted = null)
+        {
+            Mock<IPublishedContent> mock = new Mock<IPublishedContent>();
+
+            if (restricted.HasValue)
+            {
+                Mock<IPublishedProperty> property = new Mock<IPublishedProperty>();
+                property.Setup(x => x.Value).Returns(restricted.Value.ToString());
+
+                mock.Setup(x => x.GetProperty("restricted")).Returns(property.Object);
+            }
+
+            return mock.Object;
+        }
+
+        private static IPublishedContent CreateMockMember(bool authorised = false)
+        {
+            return authorised ? Mock.Of<IPublishedContent>() : null;
+        }
+    }
+}
