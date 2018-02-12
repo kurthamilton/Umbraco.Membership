656424ece6980b2228d6ef8539cc8dd0e511604c
Kurt Hamilton <khamilton@oxford-analytica.com>
2018-02-12 13:21:59 +0000

    Added GetPropertyValues extension method

--- ODK.Umbraco/Content/PublishedContentExtensions.cs
+++ ODK.Umbraco/Content/PublishedContentExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Core;
+﻿using System.Collections.Generic;
+using Umbraco.Core;
 using Umbraco.Core.Models;
 using Umbraco.Web;
 using static Umbraco.Core.Constants;
@@ -7,6 +8,17 @@ namespace ODK.Umbraco.Content
 {
     public static class PublishedContentExtensions
     {
+        public static IEnumerable<T> GetPropertyValues<T>(this IEnumerable<IPublishedContent> contents, string alias)
+        {
+            foreach (IPublishedContent content in contents)
+            {
+                if (content.HasProperty(alias))
+                {
+                    yield return content.GetPropertyValue<T>(alias);
+                }
+            }
+        }
+
         public static string ToPropertyValue(this IPublishedContent content)
         {
             return Udi.Create(UdiEntityType.Document, content.GetKey()).ToString();
