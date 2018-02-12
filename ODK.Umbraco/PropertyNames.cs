8b2b68c37c70d19baec791c472ba6efdd8175e00
Kurt Hamilton <khamilton@oxford-analytica.com>
2018-02-12 09:28:11 +0000

    Removed compiled home page settings properties

--- ODK.Umbraco/PropertyNames.cs
+++ ODK.Umbraco/PropertyNames.cs
@@ -2,16 +2,8 @@
 {
     public static class PropertyNames
     {
-        public const string LoginButtonText = "loginButtonText";
-        public const string LoginPage = "loginPage";
-        public const string LogoutButtonText = "logoutButtonText";
-        public const string RegisterButtonText = "registerButtonText";
-        public const string RegisterPage = "registerPage";
         public const string Restricted = "restricted";
-        public const string SiteLogo = "siteLogo";
         public const string SiteName = "siteName";
-        public const string ThemeColour = "themeColour";
-        public const string ThemeFontColour = "themeFontColour";
         public const string UmbracoNaviHide = "umbracoNaviHide";
     }
 }
