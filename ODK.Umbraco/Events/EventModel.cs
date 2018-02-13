88382b247050060b389d25e3fb581ba11456475b
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2018-02-13 20:34:03 +0000

    Styling changes

--- ODK.Umbraco/Events/EventModel.cs
+++ ODK.Umbraco/Events/EventModel.cs
@@ -8,6 +8,7 @@ namespace ODK.Umbraco.Events
     {
         private readonly Lazy<DateTime> _date;
         private readonly Lazy<string> _description;
+        private readonly Lazy<string> _location;
         private readonly Lazy<string> _mapQuery;
 
         public EventModel(IPublishedContent content)
@@ -18,12 +19,15 @@ namespace ODK.Umbraco.Events
 
             _date = new Lazy<DateTime>(() => content.GetPropertyValue<DateTime>(EventPropertyNames.Date));
             _description = new Lazy<string>(() => content.GetPropertyValue<string>(EventPropertyNames.Description));
+            _location = new Lazy<string>(() => content.GetPropertyValue<string>(EventPropertyNames.Location));
             _mapQuery = new Lazy<string>(() => content.GetPropertyValue<string>(EventPropertyNames.MapQuery));
         }
 
         public DateTime Date => _date.Value;
 
         public string Description => _description.Value;
+
+        public string Location => _location.Value;
 
         public string MapQuery => _mapQuery.Value;
 
