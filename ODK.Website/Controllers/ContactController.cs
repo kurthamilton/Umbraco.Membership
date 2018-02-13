407838173d522bcde6012eedc475e2843ad96884
Kurt Hamilton <khamilton@oxford-analytica.com>
2018-02-13 13:27:49 +0000

    Added contact controller prototype

---
+++ ODK.Website/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Mail;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace ODK.Website.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string email, string message)
+        {
+            IPublishedContent content = Umbraco.AssignedContentItem;
+
+            string contactEmailAddress = content.GetPropertyValue<string>("contactEmailAddress");
+
+            string body =
+                "A message has been sent through the drunkenknitwits.com website from " + email + "." +
+                "Message:" +
+                "" + message;
+
+            MailMessage mailMessage = new MailMessage("noreply@drunkenknitwits.com", contactEmailAddress, "Website contact message", body);
+
+            using (SmtpClient smtpClient = new SmtpClient())
+            {
+                smtpClient.Send(mailMessage);
+            }
+
+            TempData["Feedback"] = "Message sent";
+
+            return CurrentUmbracoPage();
+        }
+    }
+}
