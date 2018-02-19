7e997c867bb40c6add059950bdd41fefcbf574c3
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2018-02-19 08:25:57 +0000

    Added account page content, added initial payments data service

---
+++ ODK.Data/Payments/Payment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ODK.Data.Payments
+{
+    public class Payment
+    {
+        public double Amount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ ODK.Data/Payments/PaymentsDataService.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace ODK.Data.Payments
+{
+    public class PaymentsDataService
+    {
+        private readonly string _connectionString;
+
+        public PaymentsDataService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IReadOnlyCollection<Payment>> GetMemberPayments(int memberId)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (SqlCommand command = new SqlCommand("SELECT Amount, Date FROM dbo.odkPayments WHERE MemberId = @MemberId", connection))
+                {
+                    command.Parameters.Add("@MemberId", SqlDbType.Int).Value = memberId;
+
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                    List<Payment> payments = new List<Payment>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        Payment payment = ReadPayment(reader);
+                        payments.Add(payment);
+                    }
+
+                    return payments;
+                }
+            }
+        }
+
+        private static Payment ReadPayment(SqlDataReader reader)
+        {
+            return new Payment
+            {
+                Amount = reader.GetDouble(reader.GetOrdinal("Amount")),
+                Date = reader.GetDateTime(reader.GetOrdinal("Date"))
+            };
+        }
+    }
+}
