a0408f648ae828c0f132b14d212f21d843f76548
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2018-06-10 12:27:30 +0100

    Update insta URL - differentiate between api and insta

--- ODK.Website/scripts/payments.js
+++ ODK.Website/scripts/payments.js
@@ -1,22 +1,60 @@
 ﻿(function() {
     $(function () {
-        $('[data-payment-form]').on('submit', function(e) {
-            var externalForm = $(this);
-            var internalForm = $(externalForm.data('payment-form'));
+        $('.paypal-button').each(function () {
+            var button = $(this);
+            var paymentForm = button.closest('form');
+            var executeForm = $(paymentForm.data('execute-form'));
 
-            $.ajax({
-                url: internalForm.attr('action'),
-                method: 'POST',
-                data: internalForm.serialize(),
-                success: function (token) {
-                    externalForm.off('submit');
-                    $('[data-payment-token]', externalForm).val(token);
-                    externalForm.submit();
+            var payment;
+            paypal.Button.render({
+                env: 'production',
+
+                commit: true,
+
+                payment: function () {
+                    var request =
+                        post(paymentForm)
+                        .then(function (data) {
+                            payment = data;
+                            return data.id;
+                        });
+                    return request;
+                },
+
+                onAuthorize: function (data) {
+                    post(executeForm, {
+                        payerId: data.payerID,
+                        paymentId: data.paymentID
+                    })
+                    .then(function (data) {
+
+                    });
                 }
-            });
 
-            e.preventDefault();
-            return false;
+            }, button[0]);
         });
     });
+
+    function post(form, data) {
+        var formData = form.serializeArray();
+
+        if (data) {
+            for (var prop in data) {
+                if (data.hasOwnProperty(prop)) {
+                    formData.push({
+                        name: prop,
+                        value: data[prop]
+                    });
+                }
+            }
+        }
+
+        return $.when(
+            $.ajax({
+                url: form.attr('action'),
+                method: 'POST',
+                data: formData
+            })
+        );
+    }
 })();
