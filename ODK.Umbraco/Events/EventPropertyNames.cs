78e093190b2f869026d23d746cf25a829350dd1b
Kurt Hamilton <kurt.j.hamilton@gmail.com>
2018-06-11 20:17:13 +0100

    Add event image and time

--- ODK.Umbraco/Events/EventPropertyNames.cs
+++ ODK.Umbraco/Events/EventPropertyNames.cs
@@ -5,8 +5,10 @@
         public const string Address = "eventAddress";
         public const string Date = "eventDate";
         public const string Description = "eventDescription";
+        public const string Image = "eventImage";
         public const string Location = "eventLocation";
         public const string MapQuery = "eventMapQuery";
         public const string Public = "eventPublic";
+        public const string Time = "eventTime";
     }
 }
